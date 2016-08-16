49c50dce4be7c89e34dc2958536c76d0596a532e
harel.efraim <harel.e@gmail.com>
2016-08-16 18:56:07 +0300

    fix

--- docker/src/main/java/sample/Main.java
+++ docker/src/main/java/sample/Main.java
@@ -19,7 +19,7 @@ public class Main {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "Hello World - Version 1.0.3";
+            String response = "Hello World - Version 1.0.3.1";
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
