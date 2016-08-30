83516d4f9df99f14ae92e2e7b3e8634fa92d4569
harel.efraim <harel.e@gmail.com>
2016-08-30 20:33:51 +0300

    embedded redis

--- core/src/test/java/sample/EmbeddedRedisTest.java
+++ core/src/test/java/sample/EmbeddedRedisTest.java
@@ -2,13 +2,20 @@ package sample;
 
 import org.testng.annotations.Test;
 import redis.embedded.RedisServer;
+import redis.embedded.exceptions.EmbeddedRedisException;
 
 public class EmbeddedRedisTest {
     @Test
     public void testEmbedded() throws Exception {
 
         RedisServer redisServer = new RedisServer(6379);
-        redisServer.start();
+        try {
+            redisServer.start();
+        } catch (EmbeddedRedisException e) {
+            e.printStackTrace();
+            System.err.println("=======================");
+            e.getCause().printStackTrace();
+        }
 
         redisServer.stop();
 
