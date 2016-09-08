82d35846343def3cd8fbb607b766cd0c5d3a033b
harel.efraim <harel.e@gmail.com>
2016-09-08 08:30:03 +0300

    embedded redis

--- core/src/test/java/sample/EmbeddedRedisTest.java
+++ core/src/test/java/sample/EmbeddedRedisTest.java
@@ -19,4 +19,15 @@ public class EmbeddedRedisTest {
         redisServer.stop();
 
     }
+
+
+    /*
+
+/tmp/1472594360264-0/redis-server-2.8.19: /lib64/libc.so.6: version `GLIBC_2.14' not found (required by /tmp/1472594360264-0/redis-server-2.8.19)
+java.lang.RuntimeException: Can't start redis server. Check logs for details.
+       	at redis.embedded.AbstractRedisInstance.awaitRedisServerReady(AbstractRedisInstance.java:61)
+       	at redis.embedded.AbstractRedisInstance.start(AbstractRedisInstance.java:39)
+       	at redis.embedded.RedisServer.start(RedisServer.java:9)
+
+    * */
 }
